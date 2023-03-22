b6ce9e747c51b36dffc8fe73e741ac811a3e6374
sho555 <jesselo666@yahoo.com.tw>
2023-03-22 08:45:48 +0800

    upload

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/FrontLayout.vue'),
+    children: [
+      {
+        path: 'home',
+        component: () => import('../views/front/HomeView.vue')
+      },
+      {
+        path: 'about',
+        component: () => import('../views/front/AboutView.vue')
+      },
+      {
+        path: 'products',
+        component: () => import('../views/front/ProductsView.vue')
+      }, {
+        path: 'product/:id',
+        component: () => import('../views/front/ProductView.vue')
+      },
+      {
+        path: 'cart',
+        component: () => import('../views/front/CartView.vue')
+      },
+      {
+        path: 'login',
+        component: () => import('../views/front/LoginView.vue')
+      }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: () => import('../views/NotFound.vue'),
+    hidden: true
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  linkActiveClass: 'active',
+  routes
+})
+
+export default router
